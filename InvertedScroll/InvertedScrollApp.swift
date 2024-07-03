8cfc9c194672554302283731c94cd032c6ec8ddf
ribragimov-mwb <ribragimov@malwarebytes.com>
2024-07-03 09:34:26 +0300

    Initial Commit

---
+++ InvertedScroll/InvertedScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvertedScrollApp.swift
+//  InvertedScroll
+//
+//  Created by Ramill Ibragimov on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct InvertedScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
